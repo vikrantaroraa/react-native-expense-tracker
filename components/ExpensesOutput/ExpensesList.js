c69598dc943b58011f288ab7cca77494fe864274
vikrantaroraa <arora.veshu@gmail.com>
2023-11-20 17:35:19 +0530

    feat: created the ExpenseItemcomponent

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList } from "react-native";
+import ExpenseItem from "./ExpenseItem";
 
 const renderExpenseItem = (itemData) => {
-  return <Text>{itemData.item.description}</Text>;
+  return <ExpenseItem {...itemData.item} />;
 };
 
 function ExpensesList({ expenses }) {
