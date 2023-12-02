c4c2a2dd8dcc9561935c274f7389d4acd7a9b0c8
vikrantaroraa <arora.veshu@gmail.com>
2023-12-02 18:43:25 +0530

    feat: added the state variables for the ExpenseForm

--- components/ManageExpenses/ExpenseForm.js
+++ components/ManageExpenses/ExpenseForm.js
@@ -1,31 +1,55 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
 
 function ExpenseForm() {
-  const amountChangeHandler = () => {};
+  const [inputValues, setInputValues] = useState({
+    amount: "",
+    date: "",
+    description: "",
+  });
+
+  const { amount, date, description } = inputValues;
+
+  const inputChangeHandler = (inputIdentifier, enteredValue) => {
+    setInputValues((currentInputValues) => {
+      return {
+        ...currentInputValues,
+        [inputIdentifier]: enteredValue,
+      };
+    });
+  };
 
   return (
-    <View>
-      <Input
-        label={"Amount"}
-        textInputConfig={{
-          keyboardType: "decimal-pad",
-          onChangeText: amountChangeHandler,
-        }}
-      />
-      <Input
-        label={"Date"}
-        textInputConfig={{
-          placeholder: "YYYY-MM-DD",
-          maxLength: 10,
-          onChangeText: () => {},
-        }}
-      />
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expenses</Text>
+      <View style={styles.inputRow}>
+        <Input
+          label={"Amount"}
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: () => inputChangeHandler("amount"),
+            value: amount,
+          }}
+          style={styles.rowInput}
+        />
+        <Input
+          label={"Date"}
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: () => inputChangeHandler("date"),
+            value: date,
+          }}
+          style={styles.rowInput}
+        />
+      </View>
       <Input
         label={"Description"}
         textInputConfig={{
           multiline: true,
+          onChangeText: () => inputChangeHandler("description"),
+          value: description,
         }}
       />
     </View>
@@ -33,3 +57,23 @@ function ExpenseForm() {
 }
 
 export default ExpenseForm;
+
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 5,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginVertical: 20,
+    textAlign: "center",
+  },
+  inputRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
